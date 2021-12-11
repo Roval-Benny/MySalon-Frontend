d16b0eba2989ea9e5d98505bc0e88fb510028511
unknown <Austin.Francis@ust.com>
2021-12-11 17:33:39 +0530

    updated3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SalonDetailComponent } from './salon-detail/salon-detail.component';
 import { HeaderComponent } from './header/header.component';
+import { SalonListComponent } from './salon-list/salon-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SalonDetailComponent,
-    HeaderComponent
+    HeaderComponent,
+    SalonListComponent
   ],
   imports: [
     BrowserModule,
