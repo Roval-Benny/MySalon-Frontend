c534fb6f7274c5eeb0d0e112d270318dbe7e36b9
Roval Benny <rovalbenny@gmail.com>
2021-12-11 22:32:16 +0530

    Home curousal added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SalonDetailComponent } from './salon-detail/salon-detail.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SalonDetailComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
