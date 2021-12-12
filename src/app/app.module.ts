fa65f33b363d6bc7419b881805dab0da9c318365
Richu Joy <Richu.Joy@ust.com>
2021-12-12 19:41:15 +0530

    user appointments completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
+
 import { CartComponent } from './cart/cart.component';
 import { SalonDetailComponent } from './salon-detail/salon-detail.component';
 import { HeaderComponent } from './header/header.component';
 import { CheckoutComponent } from './checkout/checkout.component';
-=======
 
-import { CartComponent } from './cart/cart.component';
 
-import { SalonDetailComponent } from './salon-detail/salon-detail.component';
-import { HeaderComponent } from './header/header.component';
+
 import { HomeComponent } from './home/home.component';
 
 import { SalonListComponent } from './salon-list/salon-list.component';
 import { FooterComponent } from './footer/footer.component';
+import { UserAppointmentsComponent } from './user-appointments/user-appointments.component';
 
->>>>>>> a94f60f5c824f9fcde7bd7da78e3809d64a62576
 
 @NgModule({
   declarations: [
@@ -27,14 +24,12 @@ import { FooterComponent } from './footer/footer.component';
     CartComponent,
     SalonDetailComponent,
     HeaderComponent,
-<<<<<<< HEAD
-    CheckoutComponent
-=======
+    CheckoutComponent,
     SalonListComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    UserAppointmentsComponent
 
->>>>>>> a94f60f5c824f9fcde7bd7da78e3809d64a62576
   ],
   imports: [
     BrowserModule,
