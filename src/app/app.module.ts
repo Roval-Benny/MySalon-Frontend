f452ac07c542ff5dd7ce9914c1eada7eea60bda5
Richu Joy <Richu.Joy@ust.com>
2021-12-12 09:45:38 +0530

    checkout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { CartComponent } from './cart/cart.component';
-=======
 import { SalonDetailComponent } from './salon-detail/salon-detail.component';
 import { HeaderComponent } from './header/header.component';
->>>>>>> 55294b8871ac2c839f7ef61dcb60e5611ddca720
+import { CheckoutComponent } from './checkout/checkout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    CartComponent
-=======
+    CartComponent,
     SalonDetailComponent,
-    HeaderComponent
->>>>>>> 55294b8871ac2c839f7ef61dcb60e5611ddca720
+    HeaderComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
