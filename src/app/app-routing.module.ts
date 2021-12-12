0da24d14f17d783b4a27a1c8556eee7e48be7bd0
yashodhar <yashodhar.belgaumkar@ust.com>
2021-12-12 11:03:05 +0530

    login, registration completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { OtpComponent } from './otp/otp.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { SalonDetailComponent } from './salon-detail/salon-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'header',pathMatch: 'full'},
+  {path:'header', component: HeaderComponent},
+  {path:'salon-detail', component: SalonDetailComponent},
+  {path:'login', component: LoginComponent},
+  {path: 'otp', component: OtpComponent},
+  {path:'registration', component: RegistrationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SalonDetailComponent } from './salon-detail/salon-detail.component';
 import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { OtpComponent } from './otp/otp.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SalonDetailComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent,
+    OtpComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
