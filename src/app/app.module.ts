bdb396e713cfa8305e79f99c3b2eed03bbb40938
Roval Benny <rovalbenny@gmail.com>
2021-12-12 16:45:21 +0530

    Router-Service Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,46 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { CartComponent } from './cart/cart.component';
-import { SalonDetailComponent } from './salon-detail/salon-detail.component';
-import { HeaderComponent } from './header/header.component';
-import { CheckoutComponent } from './checkout/checkout.component';
-=======
-
-import { CartComponent } from './cart/cart.component';
-
 import { SalonDetailComponent } from './salon-detail/salon-detail.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-
 import { SalonListComponent } from './salon-list/salon-list.component';
 import { FooterComponent } from './footer/footer.component';
+import { RouterModule, Routes } from '@angular/router';
+import { RouterService } from './services/router.service';
 
->>>>>>> a94f60f5c824f9fcde7bd7da78e3809d64a62576
-
+const routes: Routes = [
+  {path:'home', component: HomeComponent},
+  { path: '', component: HomeComponent },
+  { path: 'salon-list/:id', component: SalonListComponent },
+  { path: 'salon-detail/:id', component: SalonDetailComponent },
+  { path: 'cart', component: CartComponent },
+  { path: '**', redirectTo: '' }
+]
 @NgModule({
   declarations: [
     AppComponent,
     CartComponent,
     SalonDetailComponent,
     HeaderComponent,
-<<<<<<< HEAD
-    CheckoutComponent
-=======
-    SalonListComponent,
     HomeComponent,
+    SalonListComponent,
     FooterComponent
-
->>>>>>> a94f60f5c824f9fcde7bd7da78e3809d64a62576
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes,{enableTracing:true})
   ],
-  providers: [],
+  providers: [RouterService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { RouterService } from '../services/router.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent{
 
-  constructor() { }
+  constructor(private rs:RouterService) { }
 
-  ngOnInit(): void {
+  onHome(){
+    this.rs.routeToHome();
+  }
+  onAppointment(){
+    this.rs.routeToAppointment();
+  }
+
+  onProfile(){
+    this.rs.routeToProfile();
+  }
+
+  onLocation(){
+    alert("Location");
+  }
+  
+  onLogin(){
+    this.rs.routeToLogin();
+  }
+
+  onCart(){
+    this.rs.routeToCart();
   }
 
 }
